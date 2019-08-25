326da7c7cdcd8044c58c09f07ee01ff11a0378d1
Wei Jian Gan <weijiangan@outlook.com>
2019-08-26 07:07:30 +0800

    fix bad path

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   output: {
     filename: "bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "..", "dist"),
     publicPath: "/"
   },
   module: {
